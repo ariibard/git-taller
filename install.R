f7d63d0b690919441a576d97e1a17bf886117a02
ariibard <ariibardauil@gmail.com>
2025-06-24 09:47:50 -0300

    subo todo

---
+++ install.R
@@ -0,0 +1,10 @@
+install.packages(
+  'saperlipopette', 
+  repos = c('https://packages.ropensci.org', 'https://cloud.r-project.org')
+)
+
+withr::local_language("es")
+carpeta <- withr::local_tempdir()
+saperlipopette::exo_one_small_change(carpeta)
+
+#blabla
